ec8b25af77f390524f05fd1da2e79b283da02591
aDreamCatcher <344944410@qq.com>
2017-12-06 14:35:52 +0800

    init(idevice_id/ideviceinfo)

---
+++ idevice_id/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  idevice_id
+//
+//  Created by xin on 2017/12/6.
+//  Copyright © 2017年 lgy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
